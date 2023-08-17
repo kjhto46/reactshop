5fe93d36212d2b0462216f0efdec858145b6fa53
kjhto46 <tony1996kjh@gmail.com>
2023-08-18 00:35:03 +0900

    react-router-dom라이브러리 6 사용

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import "./App.css";
 import { useState } from "react";
 import dataShoes from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   let [shoes] = useState(dataShoes);
 
   return (
     <div className="App">
+      <Link to="/">메인페이지</Link>
+      <Link to="/detail">상세페이지</Link>
+      <Routes>
+        <Route path="/" element={<div>메인페이지임</div>} />
+        <Route path="/detail" element={<div>상세페이지임</div>} />
+        <Route path="/about" element={<div>about페이지임</div>} />
+      </Routes>
+
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">리엑트쇼핑</Navbar.Brand>
+          <Navbar.Brand href="/">리엑트쇼핑</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#cart">장바구니</Nav.Link>
-            <Nav.Link href="#pricing">결제</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/cart">장바구니</Nav.Link>
+            <Nav.Link href="/pricing">결제</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
