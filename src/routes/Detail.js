e0e4206ee93d51f96804d8496679ba2250336893
kjhto46 <tony1996kjh@gmail.com>
2023-08-20 23:53:57 +0900

    Lifecycle에 대한 개념 이해와 useEffect() 다시 공부

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -6,7 +7,27 @@ let YellowBtn = styled.button`
   color: ${(props) => props.color};
 `;
 
+/**
+ * 과거 방식으로 컴포넌트 생성과 Lifecycle
+ * componentDidMount(){} //컴포넌트 mount시 여기 코드 실행
+ * componentDidUpdate(){} //컴포넌트 update시 여기 코드 실행
+ * componentWillUnmount(){} //컴포넌트 unmount시 여기 코드 실행
+ *
+ * 요즘은 useEffect() hook으로 작업이 손쉽게 가능
+ */
+// class Detail2 extends React.Component {
+//   componentDidMount() {}
+//   componentDidUpdate() {}
+//   componentWillUnmount() {}
+// }
+
 function Detail(props) {
+  useEffect(() => {
+    console.log("안녕");
+  });
+
+  let [count, setCount] = useState(0);
+
   // 현재 url에 입력한 숫자
   let { id } = useParams();
   let 찾는상품 = props.shoes.find((배열자료) => {
@@ -17,7 +38,15 @@ function Detail(props) {
     <div className="container">
       {찾는상품 ? (
         <div className="row">
-          <YellowBtn color="blue">버튼</YellowBtn>
+          <p>{count}</p>
+          <YellowBtn
+            color="red"
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            버튼
+          </YellowBtn>
           <div className="col-md-6">
             <img
               src="https://codingapple1.github.io/shop/shoes1.jpg"
