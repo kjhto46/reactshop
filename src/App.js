867f8a36458374e96c48f42bcc2039dd787d0a7f
kjhto46 <tony1996kjh@gmail.com>
2023-10-06 23:17:31 +0900

    react-query 연습

--- src/App.js
+++ src/App.js
@@ -6,27 +6,33 @@ import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./routes/Detail";
 import axios from "axios";
 import Cart from "./routes/Cart";
+import { useQuery } from "@tanstack/react-query";
 
-export let Context1 = createContext()
+export let Context1 = createContext();
 
 function App() {
-  let obj = {name : 'kim'}
-  
-  localStorage.setItem('data', JSON.stringify(obj))
-
   let [shoes, setShoes] = useState(dataShoes);
   let [재고] = useState([10, 11, 12]);
   let navigate = useNavigate();
-  let [watchedList, setWatchedList] = useState(()=>{
-    let watchList = localStorage.getItem('watched');
+
+  let [watchedList, setWatchedList] = useState(() => {
+    let watchList = localStorage.getItem("watched");
     return watchList ? JSON.parse(watchList) : [];
-  })
+  });
   useEffect(() => {
-    const watchList = localStorage.getItem('watched');
+    const watchList = localStorage.getItem("watched");
     if (watchList) {
       setWatchedList(JSON.parse(watchList));
     }
   }, []);
+  let queryResult = useQuery(["작명"], () =>
+    axios.get("https://codingapple1.github.io/userdata.json").then((a) => {
+    console.log("refetch")
+      return a.data;
+    }),
+    {staleTime : 2000} // 자동 refetch 커거나 끌 수 있음
+  ); //useQuery로 ajax를 연결하면 장점은 성공/실패시 확인, 재시도 기능 등등 추가할수있다고한다.
+
 
   return (
     <div className="App">
@@ -55,6 +61,11 @@ function App() {
               장바구니
             </Nav.Link>
           </Nav>
+          <Nav className="ms-auto" style={{ color: "#fff" }}>
+            {queryResult.isLoading && "로딩중"}
+            {queryResult.error && "에러남"}
+            {queryResult.data && queryResult.data.name}
+          </Nav>
         </Container>
       </Navbar>
 
@@ -69,39 +80,46 @@ function App() {
                   return <Card shoes={a} i={i + 1} key={i}></Card>;
                 })}
               </div>
-              <button onClick={()=>{
-                axios.get('https://codingapple1.github.io/shop/data2.json')
-                .then((result)=> {
-                console.log(result.data)
-                let copy = [...shoes, ...result.data];
-                setShoes(copy);
-                })
-                .catch(()=>{
-                  console.log('데이터 실패함')
-                })
-              }} >더보기</button>
+              <button
+                onClick={() => {
+                  axios
+                    .get("https://codingapple1.github.io/shop/data2.json")
+                    .then((result) => {
+                      console.log(result.data);
+                      let copy = [...shoes, ...result.data];
+                      setShoes(copy);
+                    })
+                    .catch(() => {
+                      console.log("데이터 실패함");
+                    });
+                }}
+              >
+                더보기
+              </button>
               <ul>
                 <li>최근 본 항목</li>
                 {watchedList.map((a, i) => {
-
-                  const foundShoe = shoes.find(shoe => shoe.id === a);
+                  const foundShoe = shoes.find((shoe) => shoe.id === a);
 
                   const nameToDisplay = foundShoe ? foundShoe.title : "";
-                  return <li key={i}>
-                    <a href={"/detail/" + a}>
-                    {nameToDisplay}
-                    </a>
-                  </li>
+                  return (
+                    <li key={i}>
+                      <a href={"/detail/" + a}>{nameToDisplay}</a>
+                    </li>
+                  );
                 })}
               </ul>
             </>
           }
         />
-        <Route path="/detail/:id" element={
-        <Context1.Provider value={{재고, shoes}}>
-          <Detail shoes={shoes} />
-        </Context1.Provider>
-        } />
+        <Route
+          path="/detail/:id"
+          element={
+            <Context1.Provider value={{ 재고, shoes }}>
+              <Detail shoes={shoes} />
+            </Context1.Provider>
+          }
+        />
         <Route path="/about" element={<About />}>
           <Route
             path="member"
@@ -139,13 +157,13 @@ function About() {
 function Card(props) {
   return (
     <div className="col-md-3">
-      <a href={"/detail/" + (props.i - 1) }>
-      <img
-        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
-        alt={"신발" + props.i}
-      />
-      <h4>{props.shoes.title}</h4>
-      <p>{props.shoes.content}</p>
+      <a href={"/detail/" + (props.i - 1)}>
+        <img
+          src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+          alt={"신발" + props.i}
+        />
+        <h4>{props.shoes.title}</h4>
+        <p>{props.shoes.content}</p>
       </a>
     </div>
   );
